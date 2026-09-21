432ac836cb358d02a8c04a60c6b082e684e1a454
Devansh Singhal <5472907devanshsinghal@gmail.com>
2026-09-22 00:30:38 +0530

    Time: 0 ms (100.00%) | Memory: 11.6 MB (32.49%) - LeetSync

---
+++ 1137-height-checker/height-checker.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int heightChecker(vector<int>& heights) {
+        vector<int>temp;
+        temp=heights;
+        sort(temp.begin(),temp.end());
+        int count=0;
+        for(int i=0; i<heights.size(); i++){
+            if(heights[i]!=temp[i]) count++;
+        }
+        return count;
+    }
+};
